5dc1e886c7701c2c9550a1909f200657b5882a4b
howardmann <howardmann27@gmail.com>
2017-02-04 18:58:04 +1100

    Add beforeModel rule

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel(){
+    this.store.findAll('todo').then((data)=> {
+      if (data.get('length') == 0) {
+        this.store.createRecord('todo',{title: 'get milk',isComplete: false}).save();
+        this.store.createRecord('todo',{title: 'pick up clothes',isComplete: false}).save();
+        this.store.createRecord('todo',{title: 'be amazing',isComplete: false}).save();
+      }
+    })
+  },
   model(){
     return this.store.findAll('todo');
   }
